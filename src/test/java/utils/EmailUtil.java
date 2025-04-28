9d6054b769f03d4e9de55c5ab9d423844c8eb47d
ankithhhh <114284384+ankithhhh@users.noreply.github.com>
2025-04-28 17:39:52 +0530

    Update EmailUtil.java

--- src/test/java/utils/EmailUtil.java
+++ src/test/java/utils/EmailUtil.java
@@ -22,7 +22,7 @@ public class EmailUtil {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.host", "smtp.office365.com");
         props.put("mail.smtp.port", "587");
 
         Session session = Session.getInstance(props, new Authenticator() {
